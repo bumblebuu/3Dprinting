cb391abddd2b91383f7f3a4fcfed8870c4b9eaef
Pál Sára <pal.sara.ajandek@gmail.com>
2019-11-06 19:15:29 +0100

    models again

--- express-print3D/models/orders.model.js
+++ express-print3D/models/orders.model.js
@@ -1,36 +1,43 @@
-module.exports = function (mongoose) {
-  return [{
-    insdate: {
-      type: Date,
-      unique: false,
-      required: true,
-    },
-    userid: {
-      type: Number,
-      unique: false,
-      required: true,
-    },
-    productid: {
-      type: Number,
-      unique: false,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      unique: false,
-      required: true,
-    },
-    unitprice: {
-      type: Number,
-      unique: false,
-      required: true,
-    },
-    status: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-  }, {
-    timestamps: true,
-  }];
-};
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const ordersSchema = new Schema({
+  insdate: {
+    type: Date,
+    unique: false,
+    required: true,
+    default: Date.now(),
+  },
+  userid: {
+    type: Number,
+    unique: false,
+    required: true,
+  },
+  productid: {
+    type: Number,
+    unique: false,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    unique: false,
+    required: true,
+  },
+  unitprice: {
+    type: Number,
+    unique: false,
+    required: true,
+  },
+  status: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const Order = mongoose.model('orders', ordersSchema);
+module.exports = Order;
--- express-print3D/models/products.model.js
+++ express-print3D/models/products.model.js
@@ -1,52 +1,56 @@
-module.exports = function (mongoose) {
-  return [{
-    name: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    seo: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    brand: {
-      type: String,
-      unique: false,
-      required: false,
-    },
-    description: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-    img: {
-      type: [String],
-      unique: false,
-      required: true,
-    },
-    menu: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-    category: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-    price: {
-      type: Number,
-      unique: false,
-      required: true,
-    },
-    isactive: {
-      type: Boolean,
-      unique: false,
-      required: false,
-    },
-  }, {
-    timestamps: true,
-    strict: false,
-  }];
-};
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const productsSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  seo: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  brand: {
+    type: String,
+    unique: false,
+    required: false,
+  },
+  description: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  img: {
+    type: [String],
+    unique: false,
+    required: true,
+  },
+  menu: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  category: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  price: {
+    type: Number,
+    unique: false,
+    required: true,
+  },
+  isactive: {
+    type: Boolean,
+    unique: false,
+    required: false,
+  },
+});
+
+const Product = mongoose.model('products', productsSchema);
+
+module.exports = Product;
