56cd81ff816d412f497b8e7fffdc348983cea617
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-08 15:38:03 +0100

    pagination

--- express-print3D/routes/products.js
+++ express-print3D/routes/products.js
@@ -6,23 +6,44 @@ const router = express.Router();
 const Product = require('../models/products.model');
 
 // get products
-router.get('/', (req, res) => {
-  Product.find((err, products) => {
-    res.render('products', {
-      products,
-    });
-  });
+
+router.get('/', (req, res, next) => {
+  res.redirect('http://localhost:3000/products/1');
 });
 
-// get single article
-router.get('/:seo', (req, res) => {
-  Product.findOne({
-    seo: req.params.seo,
-  }, (err, product) => {
-    res.render('product', {
-      product,
+router.get('/:page', async (req, res, next) => {
+  const perPage = 6;
+  const page = req.params.page || 1;
+  // const skipMethod = ((perPage * page) - perPage);
+
+  Product
+    .find({})
+    .skip((perPage * page) - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+        res.render('products', {
+          products,
+          current: page,
+          pages: Math.ceil(count / perPage),
+        });
+      });
     });
-  });
 });
+
+
+// // get single article
+// router.get('/:seo', (req, res) => {
+//   if (req.params.seo === String) {
+//     Product.findOne({
+//       seo: req.params.seo,
+//     }, (err, product) => {
+//       res.render('product', {
+//         product,
+//       });
+//     });
+//   }
+// });
 
 module.exports = router;
