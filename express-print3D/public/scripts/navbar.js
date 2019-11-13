47538e69087077d6c42639cd60a7dff4d688e92d
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-13 21:48:28 +0100

    header with lil stroke bug

--- express-print3D/public/scripts/navbar.js
+++ express-print3D/public/scripts/navbar.js
@@ -2,12 +2,16 @@
 function openNav() {
   document.getElementById('mySidebar').style.width = '250px';
   document.getElementById('main').style.marginRight = '250px';
+  document.getElementById('dark-page').style.marginRight = '180px';
+  document.getElementById('picture').style.marginLeft = '70px';
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('mySidebar').style.width = '0';
   document.getElementById('main').style.marginRight = '0';
+  document.getElementById('dark-page').style.marginRight = '0';
+  document.getElementById('picture').style.marginLeft = '0';
 }
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
