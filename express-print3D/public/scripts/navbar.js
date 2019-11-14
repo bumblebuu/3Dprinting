24fdc7f1d77efa9c36c84fb1c6aa047ad6712300
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-14 21:11:12 +0100

    nav

--- express-print3D/public/scripts/navbar.js
+++ express-print3D/public/scripts/navbar.js
@@ -1,14 +1,14 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById('mySidebar').style.width = '250px';
-  document.getElementById('main').style.marginRight = '250px';
-  document.getElementById('dark-page').style.marginRight = '180px';
+  document.getElementById('mySidebar').style.right = '0';
+  document.getElementById('main').style.marginRight = '300px';
+  document.getElementById('dark-page').style.marginRight = '50px';
   document.getElementById('picture').style.marginLeft = '70px';
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById('mySidebar').style.width = '0';
+  document.getElementById('mySidebar').style.right = '-300px';
   document.getElementById('main').style.marginRight = '0';
   document.getElementById('dark-page').style.marginRight = '0';
   document.getElementById('picture').style.marginLeft = '0';
