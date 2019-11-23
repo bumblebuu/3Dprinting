d87c1eaddc268e9780cfd6f4dd7d9616a2f7a416
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-23 18:03:00 +0100

    if-else button trans

--- express-print3D/public/scripts/navbar.js
+++ express-print3D/public/scripts/navbar.js
@@ -1,18 +1,25 @@
+const loginBtn = document.getElementById('login-btn');
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById('mySidebar').style.right = '0';
-  document.getElementById('user-pic-div').style.left = '75%';
-  document.getElementById('login-btn').style.left = '73%';
   document.getElementById('picture').style.width = '35vw';
-  document.querySelector('.dropdown-user').style.display = 'none';
+  if (loginBtn) {
+    loginBtn.style.left = '73%';
+  } else {
+    document.getElementById('user-pic-div').style.left = '75%';
+    document.querySelector('.dropdown-user').style.display = 'none';
+  }
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('mySidebar').style.right = '-300px';
-  document.getElementById('user-pic-div').style.left = '92%';
-  document.getElementById('login-btn').style.left = '90%';
   document.getElementById('picture').style.width = '50vw';
+  if (loginBtn){
+    loginBtn.style.left = '90%';
+  } else {
+    document.getElementById('user-pic-div').style.left = '92%';
+  }
 }
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
