a4acd9728dba76c354ce022ff0bb27f17553fb8b
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-27 19:12:03 +0100

    sticky nav, stupid css

--- express-print3D/public/scripts/navbar.js
+++ express-print3D/public/scripts/navbar.js
@@ -2,24 +2,20 @@ const loginBtn = document.getElementById('login-btn');
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById('mySidebar').style.right = '0';
-  document.getElementById('picture').style.width = '35vw';
-  if (loginBtn) {
-    loginBtn.style.left = '73%';
-  } else {
-    document.getElementById('user-pic-div').style.left = '75%';
-    document.querySelector('.dropdown-user').style.display = 'none';
-  }
+  document.querySelector('.header').style.marginRight = '300px';
+  document.querySelector('nav').style.marginRight = '300px';
+  document.querySelector('.openbtn').style.display = 'none';
+  document.querySelector('.dropdown-user').style.display = 'none';
+  document.querySelector('.dropdown-user').disabled = 'disabled';
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('mySidebar').style.right = '-300px';
-  document.getElementById('picture').style.width = '50vw';
-  if (loginBtn){
-    loginBtn.style.left = '90%';
-  } else {
-    document.getElementById('user-pic-div').style.left = '92%';
-  }
+  document.querySelector('.header').style.marginRight = '0';
+  document.querySelector('nav').style.marginRight = '0';
+  document.querySelector('.openbtn').style.display = 'block';
+  document.querySelector('.dropdown-user').disabled = 'enabled';
 }
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
