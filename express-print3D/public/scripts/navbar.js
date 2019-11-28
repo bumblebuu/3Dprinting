10add8d84679468d537d741a20ecdf26f461c19f
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-11-28 12:42:25 +0100

    navbar login the good place

--- express-print3D/public/scripts/navbar.js
+++ express-print3D/public/scripts/navbar.js
@@ -1,24 +1,21 @@
-const loginBtn = document.getElementById('login-btn');
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+
 function openNav() {
   document.getElementById('mySidebar').style.right = '0';
   document.querySelector('.header').style.marginRight = '300px';
   document.querySelector('nav').style.marginRight = '300px';
   document.querySelector('.openbtn').style.display = 'none';
   document.querySelector('.dropdown-user').style.display = 'none';
-  document.querySelector('.dropdown-user').disabled = 'disabled';
+  document.querySelector('.dropdown-user').setAttribute.disabled = 'disabled';
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('mySidebar').style.right = '-300px';
   document.querySelector('.header').style.marginRight = '0';
   document.querySelector('nav').style.marginRight = '0';
   document.querySelector('.openbtn').style.display = 'block';
-  document.querySelector('.dropdown-user').disabled = 'enabled';
+  document.querySelector('.dropdown-user').setAttribute.disabled = 'enabled';
 }
 
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 const dropdown = document.getElementsByClassName('dropdown-btn');
 let i;
 
