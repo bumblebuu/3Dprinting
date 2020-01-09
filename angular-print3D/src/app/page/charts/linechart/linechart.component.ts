1f094a4086db1c774c836e64abd3b1d4e508349f
Pál Sára <pal.sara.ajandek@gmail.com>
2020-01-09 21:55:13 +0100

    index carousel

--- angular-print3D/src/app/page/charts/linechart/linechart.component.ts
+++ angular-print3D/src/app/page/charts/linechart/linechart.component.ts
@@ -11,7 +11,7 @@ import { ChartService } from 'src/app/services/chart.service';
 export class LinechartComponent implements OnInit {
   thisYear: number;
 
-  public lineChartData: ChartDataSets[] = [{ data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2019' }];
+  public lineChartData: ChartDataSets[] = [{ data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2019' }, { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2020' }];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   public lineChartOptions: ChartOptions = {
     responsive: true,
@@ -21,6 +21,10 @@ export class LinechartComponent implements OnInit {
       borderColor: 'black',
       backgroundColor: 'rgba(255,0,0,0.3)',
     },
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(0,255,0,0.3)',
+    },
   ];
   public lineChartLegend = true;
   public lineChartType: ChartType = 'line';
@@ -29,14 +33,16 @@ export class LinechartComponent implements OnInit {
   constructor(private cs: ChartService) {
     this.thisYear = new Date().getFullYear();
     this.cs.readData('orders').subscribe(data => {
-      this.lineChartData = [{ data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2019' }];
+
+      this.lineChartData = [{ data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2019' }, { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], label: '2020' },];
       data.forEach(item => {
-        new Date(item.insdate).getFullYear() === this.thisYear ? this.lineChartData[0].data[new Date(item.insdate).getMonth()] += item.unitprice : item;
+        new Date(item.insdate).getFullYear() === 2019 ? this.lineChartData[0].data[new Date(item.insdate).getMonth()] += item.unitprice : item;
+        new Date(item.insdate).getFullYear() === this.thisYear ? this.lineChartData[1].data[new Date(item.insdate).getMonth()] += item.unitprice : item;
       })
     })
-}
+  }
 
-ngOnInit() {
-}
+  ngOnInit() {
+  }
 
 }
--- express-print3D/routes/index.js
+++ express-print3D/routes/index.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const Products = require('../models/products.model');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express',
-    user: req.user,
-  });
+  let productCarousel = [];
+  let productNum = 0;
+  Products.find({
+    isactive: true,
+  }, (err, products) => {
+    if (err) next(err);
+    while (productCarousel.length < 3) {
+      productNum = Math.floor(Math.random() * products.length + 1)
+      if (productCarousel.indexOf(products[productNum]) === -1) {
+        productCarousel.push(products[productNum])
+      }
+    }
+    res.render('index', {
+      title: 'Express',
+      user: req.user,
+      productCarousel,
+    });
+  })
 });
 
-router.post('/newsletter', (req, res, next) =>{
+router.post('/newsletter', (req, res, next) => {
   const emailAddress = req.body.newsLetterEmail;
 
-  
+
 })
 
 module.exports = router;
