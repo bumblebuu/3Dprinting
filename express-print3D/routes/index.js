cf41fc1c67c2b699d46cf3a9a5d541531d759110
Pál Sára <pal.sara.ajandek@gmail.com>
2020-01-12 22:43:07 +0100

    admin index started

--- express-print3D/routes/index.js
+++ express-print3D/routes/index.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
     if (err) next(err);
     while (productCarousel.length < 3) {
       productNum = Math.floor(Math.random() * products.length + 1)
-      if (productCarousel.indexOf(products[productNum]) === -1) {
+      if (productCarousel.indexOf(products[productNum]) === -1 && products[productNum] !== undefined) {
         productCarousel.push(products[productNum])
       }
     }
