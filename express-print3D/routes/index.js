ed7b41b205cc3c0f32e1d745b600f1c51cfaace2
Rebeka Grosics <rebeka.grosics@gmail.com>
2020-01-14 16:27:56 +0100

    newsletter

--- express-print3D/routes/index.js
+++ express-print3D/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require('../models/users.model');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', {
@@ -10,10 +12,22 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.post('/newsletter', (req, res, next) =>{
+router.get('/newsletter', async (req, res, next) => {
+  let modal = 'Thank you for subscribing!';
   const emailAddress = req.body.newsLetterEmail;
 
-  
-})
+  const userEmail = await User.findOne({
+    email: emailAddress,
+  });
+  if (userEmail) {
+    modal = 'This email is already subscribed, thank you!'
+  }
+
+  res.render('index', {
+    title: 'Express',
+    user: req.user,
+    modal,
+  });
+});
 
 module.exports = router;
