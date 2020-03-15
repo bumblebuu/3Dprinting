76aeb139adab653e83ea283b948d4f9a5e24f3c8
Rebeka Grosics <rebeka.grosics@gmail.com>
2020-03-15 19:16:35 +0100

    forgotten password page, email

--- express-print3D/routes/login.js
+++ express-print3D/routes/login.js
@@ -8,7 +8,13 @@ const TokenGenerator = require('uuid-token-generator');
 
 const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
 
+const handlebars = require('handlebars');
+
 const User = require('../models/users.model');
+
+const transporter = require('../nodeMailerWithTemp');
+
+const fs = require('fs');
 
 
 /* GET home page. */
@@ -57,6 +63,87 @@ router.post('/', async (req, res, next) => {
     title: 'Login',
     show2: true,
   });
+});
+
+
+router.get('/forgot', async (req, res, next) => {
+  res.render('forgot', {
+    title: 'Forgotten',
+  })
+});
+
+router.post('/forgot', async (req, res, next) => {
+  const emailAddress = req.body.email;
+  console.log(emailAddress);
+
+  await User.findOne({
+    email: emailAddress
+  }, (err, obj) => {
+    if (err) next(err);
+    console.log(obj);
+    const user = obj;
+    if (obj === null) {
+      return res.render('forgot', {
+        title: 'Forgotten',
+        show: true
+      })
+    } else {
+
+      const token = tokgen.generate().toString().slice(0, 9);
+
+      const readHTMLFile = function (path, callback) {
+        fs.readFile(path, {
+          encoding: 'utf-8'
+        }, function (err, html) {
+          if (err) {
+            throw err;
+            callback(err);
+          } else {
+            callback(null, html);
+          }
+        });
+      };
+
+      readHTMLFile(__dirname + './../templates/forgotten-password.html', function (err, html) {
+        const template = handlebars.compile(html);
+        const replacements = {
+          firstname: user.firstname,
+          password: token,
+          username: user.username,
+        };
+        const htmlToSend = template(replacements);
+
+        const mailOptions = {
+          from: 'beyond.paper.webshop@gmail.com',
+          to: emailAddress,
+          subject: 'Forgotten password',
+          html: htmlToSend,
+        };
+  
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(`Email sent: ${info.response}`);
+          }
+        });
+      })
+
+      User.updateOne(
+        {email: emailAddress}, {
+          password: sha1(token),
+        }, (err, obj) => {
+          if (err) next(err);
+          console.log(obj);
+        },
+      );
+
+      res.render('login', {
+        title: 'Login',
+      });
+    }
+  });
+
 });
 
 module.exports = router;
